3641093c13a56149f13f009fa478afffbda05d71
Liam Isaacs <yeqiu194@gmail.com>
2024-01-15 16:08:14 -0800

    fix: Admin enabled for pull from ACR

--- acr.tf
+++ acr.tf
@@ -5,6 +5,6 @@ resource "azurerm_container_registry" "acr" {
   name                = "${var.prefix}ContainerRegistry"
   resource_group_name = "${azurerm_resource_group.rg.name}"
   location            = "${azurerm_resource_group.rg.location}"
-  sku                 = "Basic"
-  admin_enabled       = false  
+  sku                 = "Standard"
+  admin_enabled       = true  
 }
