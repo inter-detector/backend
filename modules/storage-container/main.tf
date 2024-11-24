e3b2806c0abd6f084423351f2dc6192ab46d6ea8
Liam Isaacs <yeqiu194@gmail.com>
2024-11-24 18:25:52 -0500

    feat(storage): Add storage container and account

--- modules/storage-container/main.tf
+++ modules/storage-container/main.tf
@@ -1,13 +1,18 @@
-# resource "azurerm_storage_account" "storageacc" {
-#   name                     = "${var.prefix}storageacc"
-#   resource_group_name      = "${azurerm_resource_group.rg.name}"
-#   location                 = "${azurerm_resource_group.rg.location}"
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-# }
+locals {
+  app = var.append_workspace ? "${var.app}${title(terraform.workspace)}" : var.app
+}
 
-# resource "azurerm_storage_container" "storagecontainer" {
-#   name                  = "content"
-#   storage_account_name  = azurerm_storage_account.storageacc.name
-#   container_access_type = "private"
-# }
+
+resource "azurerm_storage_account" "storage_acc" {
+  name                     = local.app
+  resource_group_name      = var.resource_group_name
+  location                 = var.resource_group_location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "storage_container" {
+  name                  = "content"
+  storage_account_name  = azurerm_storage_account.storage_acc.name
+  container_access_type = "private"
+}
---
+++ modules/storage-container/output.tf
@@ -0,0 +1,7 @@
+output "output" {
+  value = {
+    storage_container_name             = azurerm_storage_container.storage_container.name
+    storage_account_name               = azurerm_storage_account.storage_acc.name
+    storage_account_primary_access_key = azurerm_storage_account.storage_acc.primary_access_key
+  }
+}
