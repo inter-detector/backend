ed459359e75119944c39bdcd612d1676f543c785
Liam Isaacs <yeqiu194@gmail.com>
2024-11-26 15:40:32 -0500

    doc: `admin_enabled` feature can be flipped to `true` to test in console

--- modules/acr/main.tf
+++ modules/acr/main.tf
@@ -10,7 +10,18 @@ resource "azurerm_container_registry" "acr" {
   name                = local.app
   resource_group_name = var.resource_group_name
   location            = var.resource_group_location
-  # The SKU name of the container registry. Possible values are Basic, Standard and Premium.
+  # The SKU name of the container registry. Possible values
+  # are: Basic, Standard and Premium.
   # Here we choose basic to cut costs :)
   sku                 = "Basic"
+  # The admin account is needed when you use the Azure portal
+  # to deploy a container image from a registry directly.
+  # If you need to debug the container in console, this can
+  # be flipped to True. Otherwise, you will get something that
+  # says "Admin user must first be enabled for this registry
+  # in order to access the image during the container instance creation"
+  # on the "Registry" drop down when trying to create a container
+  # instance. Read more about this here:
+  # https://learn.microsoft.com/en-us/azure/container-registry/container-registry-authentication?tabs=azure-cli#admin-account
+  admin_enabled       = false
 }
